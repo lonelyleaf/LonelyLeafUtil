01e3d7abfff1d2a065af518d5711e49a1bd04fd4
Rock <499904394@qq.com>
2016-03-29 10:36:05 +0800

    add new zeroes regexp

--- mylib/src/main/java/lonelyleaf/util/RegExpUtil.java
+++ mylib/src/main/java/lonelyleaf/util/RegExpUtil.java
@@ -11,14 +11,11 @@ public class RegExpUtil {
     private RegExpUtil() {
     }
 
-    private static Pattern pCoord;
-
     /**
      * 检查是否是位置坐标
      */
     public static boolean isCoord(String coord) {
-        if (pCoord == null)
-            pCoord = Pattern.compile("\\d+\\.+\\d+,\\d+\\.+\\d+");
+        Pattern pCoord = Pattern.compile("\\d+\\.+\\d+,\\d+\\.+\\d+");
         return pCoord.matcher(coord).matches();
     }
 
@@ -28,10 +25,17 @@ public class RegExpUtil {
      * 检查是否是身份证号
      */
     public static boolean isIdNum(String idNum) {
-        if (pIdNum == null) {
-            pIdNum = Pattern.compile("^(\\d{15}$|^\\d{18}$|^\\d{17}(\\d|X|x))$");
-        }
+        Pattern pIdNum = Pattern.compile("^(\\d{15}$|^\\d{18}$|^\\d{17}(\\d|X|x))$");
         return pIdNum.matcher(idNum).matches();
+    }
+
+
+    /**
+     * 检查是否是0序列，如0000000000
+     */
+    public static boolean isZeroes(String s) {
+        Pattern pZeroes = Pattern.compile("[0]+");
+        return pZeroes.matcher(s).matches();
     }
 
 }
